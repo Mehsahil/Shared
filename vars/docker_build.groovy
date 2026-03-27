a97520844dfc41ba6687cf9bdbca7e239165004c
Dhiraj <work.dhiraj22@gmail.com>
2026-03-27 21:43:16 +0530

    Update docker_build.groovy
    corrected cmd

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call (String ImageName, String ImageTag , String DockerUser) {
-    sh "docker build -t ${DockerUser}/${ImageName}:${ImageTag}"
+    sh "docker build -t ${DockerUser}/${ImageName}:${ImageTag} ."
 }
